50b7094cae55e05efad0724a12ceda7d4f1cabc2
Maverick95 <nickemmerson619@hotmail.com>
2020-12-27 03:03:50 -0800

    2020-12-27 Final update before incorporating Jest unit tests

--- equation-stack.js
+++ equation-stack.js
@@ -1,60 +1,4 @@
-class Stack {
-
-    constructor() {
-
-        this.last = null;
-        this.count = 0;
-
-    }
-
-    Push(value) {
-
-        let stack_new = {
-            value: value,
-            next: this.last
-        };
-
-        this.last = stack_new;
-        this.count++;
-    }
-
-    Pop() {
-
-        if (this.last) {
-
-            let {value, next} = this.last;
-            this.last.next = null;
-            this.last = next;
-            this.count--;
-            return value;
-
-        }
-
-        return null;
-
-    }
-
-    Empty() {
-
-        while(this.last) {
-            this.pop();
-        }
-
-    }
-
-    Peek() {
-
-	return this.last && this.last.value;
-
-    }
-
-    Count() {
-
-        return this.count;
-
-    }
-
-}
+import Stack from './stack.js';
 
 export default class EquationStack {
 
@@ -72,13 +16,25 @@ export default class EquationStack {
 
     }
 
-    Output = () => this.equation;
+    Output() {
+        
+        return this.equation;
+
+    }
     
-    Valid = () => this.stack_numbers.Count() === 1 && !this.stack_operators.Count();
+    Valid() {
+        
+        return this.stack_numbers.Count() === 1 && !this.stack_operators.Count();
 
-    Value = () => this.value;
+    }
 
-    Add = (n) => {
+    Value() {
+        
+        return this.value;
+
+    }
+
+    Add(n) {
 
         this.equation += ( this.equation ? ' ' : '') + n; 
 
@@ -120,6 +76,6 @@ export default class EquationStack {
             this.value = this.stack_numbers.Peek();
         }
 
-    };
+    }
 
 }
